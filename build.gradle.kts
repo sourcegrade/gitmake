66052e77af2e71f74a8a8342237eb94647933a74
Alexander Staeding <alexanderstaeding@gmail.com>
2022-03-22 19:31:49 +0100

    Fix group and version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "org.sourcegrade"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
